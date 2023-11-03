3dfb2afe92274f5cfce93bfe0ceeaf54e977557c
jonnity <jonny1996_ty@yahoo.co.jp>
2023-11-03 10:10:09 +0900

    app直下にlayout.tsxを配置しないと他のディレクトリに適用されなかったので修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="jp">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex w-full flex-col items-center justify-between  px-4 py-12">
+          <header className="w-full">
+            <h1 className="text-4xl">jonnity&apos;s site</h1>
+          </header>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
