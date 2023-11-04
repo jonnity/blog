bb201b05e7de440ef7762438261e08fdcc5e8110
jonnity <jonny1996_ty@yahoo.co.jp>
2023-11-04 11:27:50 +0900

    大まかなレイアウトを作成

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -2,8 +2,11 @@ import { Articles } from "./components/Articles";
 
 export default function Home() {
   return (
-    <section className="w-full p-3">
-      <Articles />
+    <section className="w-full">
+      <h2 className="text-2xl">記事一覧</h2>
+      <div className="py-1">
+        <Articles />
+      </div>
     </section>
   );
 }
