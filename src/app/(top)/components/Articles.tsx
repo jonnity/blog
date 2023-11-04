9d3cfc0eba5b9cb34cd0f04623474271dcc207fd
jonnity <jonny1996_ty@yahoo.co.jp>
2023-11-04 15:39:57 +0900

    ArticleのLinkを変なstyleで対応するのをやめた

--- src/app/(top)/components/Articles.tsx
+++ src/app/(top)/components/Articles.tsx
@@ -6,21 +6,19 @@ export function Articles() {
   const entries = Entry.getDiplayedEntriesList();
 
   return entries.map((entry) => (
-    <article
-      className="relative border border-solid border-slate-600 p-1"
-      key={entry.slug}
-    >
-      <Link
-        href={`./blog/${entry.slug}`}
-        className="absolute inset-0 z-10"
-      ></Link>
-      <p className="text-xl">{entry.metadata.title}</p>
-      <p>created at {entry.metadata.createdAt.toLocaleDateString()}</p>
-      <p>
-        {entry.metadata.tags.length > 0
-          ? `tags: ${entry.metadata.tags.join(", ")}`
-          : "no tags"}
-      </p>
-    </article>
+    <Link href={`./blog/${entry.slug}`}>
+      <article
+        className="relative border border-solid border-slate-600 p-1"
+        key={entry.slug}
+      >
+        <p className="text-xl">{entry.metadata.title}</p>
+        <p>created at {entry.metadata.createdAt.toLocaleDateString()}</p>
+        <p>
+          {entry.metadata.tags.length > 0
+            ? `tags: ${entry.metadata.tags.join(", ")}`
+            : "no tags"}
+        </p>
+      </article>
+    </Link>
   ));
 }
