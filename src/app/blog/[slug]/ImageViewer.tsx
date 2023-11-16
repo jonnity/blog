97b98edaeb9ba2a5ef931f88c043f4cb0a21bcee
jonnity <jonny1996_ty@yahoo.co.jp>
2023-11-16 13:22:48 +0900

    pタグの下にdivを表示することになっていたので修正

--- src/app/blog/[slug]/ImageViewer.tsx
+++ src/app/blog/[slug]/ImageViewer.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { z } from "zod";
 
 type Props = React.ClassAttributes<HTMLImageElement> &
@@ -10,8 +9,8 @@ export const ImageViewer: React.FC<Props> = (props) => {
   const src = srcSchema.parse(props.src);
   const alt = altSchema.parse(props.alt);
   return (
-    <div className="relative flex w-full justify-center">
+    <span className="relative flex w-full justify-center">
       <img src={src} alt={alt} />
-    </div>
+    </span>
   );
 };
