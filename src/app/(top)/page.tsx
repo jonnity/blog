ed20225017706eb3e65127e12b053f759bbbd7ae
jonnity <jonny1996_ty@yahoo.co.jp>
2023-12-11 23:20:52 +0900

    トップページの内容を精査

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -1,31 +1,55 @@
+import Link from "next/link";
+
 import { Articles } from "./components/Articles";
 import { SNSLogo } from "@/util/profile/SNSLogo";
 
 export default function Home() {
   return (
-    <div className="m-4 flex flex-col justify-center lg:mx-0 lg:flex-row">
-      <main className="mb-4 flex w-full rounded border border-gray-300 bg-gray-50 p-4 shadow-xl lg:w-3/5 xl:w-1/2">
-        <section className="w-full">
+    <div className="flex w-full justify-center">
+      <main className="my-2 flex w-11/12 flex-col gap-4 rounded border border-gray-300 bg-gray-50 p-4 shadow-xl lg:m-8 lg:w-3/5 xl:w-1/2">
+        <section>
+          <h2 className="flex items-center gap-4 text-2xl">
+            <span>About</span>
+            <img
+              src="/logo_keybourd.svg"
+              alt="jonnity (with logo)"
+              className="h-10"
+            />
+            <div className="flex items-center gap-2">
+              <SNSLogo serviceName="github" />
+              <SNSLogo serviceName="x" />
+              <SNSLogo serviceName="threads" />
+            </div>
+          </h2>
+          <div className="p-4">
+            <p>
+              個人開発やアリ飼育など、趣味のアウトプットの場としてこのサイトを作りました。ゆゆ式が好きです。
+            </p>
+            <p>
+              ポケモンカード公式のデッキ作成ツールのデッキコードを管理できるツール
+              (
+              <Link
+                href="https://pokeca-deck-manager.com"
+                className="font-semibold text-blue-600"
+                target="_blank"
+              >
+                ポケカデッキマネージャー
+              </Link>
+              )
+              を作ったりしてましたが、ポケモンカードが買えなかったりで、カード自体もツールのメンテもどちらもやってません。
+            </p>
+            <p>
+              今、飼いアリの中で一番愛おしいのは、アズマオオズアリのコロニーです。
+            </p>
+          </div>
+        </section>
+        <section>
           <h2 className="text-2xl">記事一覧</h2>
-          <div className="py-1">
+          <div className="p-4">
             <Articles />
           </div>
         </section>
       </main>
-
-      <aside className="m-0 w-full place-self-start rounded border border-gray-300 bg-gray-50 p-2 shadow-xl lg:mx-4 lg:w-1/4 xl:w-1/5">
-        <div className="flex items-center gap-2">
-          <h2 className="pr-1 text-lg">About jonnity</h2>
-          <SNSLogo serviceName="github" />
-          <SNSLogo serviceName="x" />
-          <SNSLogo serviceName="threads" />
-        </div>
-        <hr className="my-1" />
-        <p>個人開発やアリ飼育などをして生きています。ゆゆ式が好きです。</p>
-        <p>
-          このサイトは、エンジニア的なアウトプットを中心とした雑多な文章を自由に書ける場所として作りました。
-        </p>
-      </aside>
     </div>
   );
 }
