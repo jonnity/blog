545234f7a8553617d2fca7c546902a52ec6f3bd7
jonnity <jonny1996_ty@yahoo.co.jp>
2023-12-16 22:51:44 +0900

    draftでdescriptionが確認できなかったので修正

--- src/util/entry/Entry.ts
+++ src/util/entry/Entry.ts
@@ -4,6 +4,7 @@ import * as fs from "node:fs";
 import frontMatter from "front-matter";
 import { z } from "zod";
 import { EnvCollector } from "../EnvCollector";
+import { defaultDescription } from "../metadata";
 
 const pastDateStringSchema = z.string().transform((dateStr, ctx) => {
   const inputtedDate = new Date(dateStr);
@@ -34,12 +35,14 @@ const defaultParam = {
   title: "default title",
   createdAt: new Date(0),
   tags: [],
+  description: defaultDescription,
 };
 const metadataWithCatch = z
   .object({
     title: titleSchema.catch(defaultParam.title),
     createdAt: pastDateStringSchema.catch(defaultParam.createdAt),
     tags: tagsSchema.catch(defaultParam.tags),
+    description: descriptionSchema,
   })
   .catch(defaultParam);
 export class Entry {
