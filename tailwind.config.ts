f60f211fb3dd82486d01288c44eb09f0884f82f3
jonnity <jonny1996_ty@yahoo.co.jp>
2023-12-16 11:40:55 +0900

    背景画像をありの巣風に

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "ant-nest": "url('/bg-ant-nest.webp')",
       },
     },
   },
