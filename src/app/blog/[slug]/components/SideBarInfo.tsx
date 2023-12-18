7a4cca98d1359911254aa681eb00fa4f074d92f7
jonnity <jonny1996_ty@yahoo.co.jp>
2023-12-18 23:01:11 +0900

    サイドバーも一つのコンポーネントにまとめて、pageを整理

---
+++ src/app/blog/[slug]/components/SideBarInfo.tsx
@@ -0,0 +1,28 @@
+import { SNSLogo } from "@/util/profile/SNSLogo";
+import { EntryProp } from "@/util/entry/Entry";
+
+export const SideBarInfo: React.FC<EntryProp> = ({ entry }) => {
+  return (
+    <>
+      <Profile />
+    </>
+  );
+};
+
+const Profile: React.FC = () => (
+  <>
+    <div className="flex items-center gap-2">
+      <h2 className="pr-1 text-lg">About jonnity</h2>
+      <SNSLogo serviceName="github" />
+      <SNSLogo serviceName="x" />
+      <SNSLogo serviceName="threads" />
+    </div>
+    <hr className="my-1 h-px border-0 bg-gray-400" />
+    <div className="p-2">
+      <p>個人開発やアリ飼育などをして生きています。ゆゆ式が好きです。</p>
+      <p>
+        このサイトは、エンジニア的なアウトプットを中心とした雑多な文章を自由に書ける場所として作りました。
+      </p>
+    </div>
+  </>
+);
