e04caee9de1d2ee240c0b8bfa20e7d53b4887178
jonnity <jonny1996_ty@yahoo.co.jp>
2023-12-18 22:12:35 +0900

    blogページのlayoutではヘッダーを指定するのみに

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { SNSLogo } from "@/util/profile/SNSLogo";
 const logoImagePath = "/logo_keybourd.svg";
 
 export default function BlogPageLayout({
@@ -26,27 +25,7 @@ export default function BlogPageLayout({
           <p className="invisible self-end">category: hoge</p>
         </div>
       </header>
-      <div className="m-4 flex flex-col justify-center lg:mx-0 lg:flex-row">
-        <main className="contents-base mb-4 flex w-full p-4  lg:w-3/5 xl:w-1/2">
-          <section className="w-full">{children}</section>
-        </main>
-
-        <aside className="contents-base m-0 w-full place-self-start p-2 lg:mx-4 lg:w-1/4 xl:w-1/5">
-          <div className="flex items-center gap-2">
-            <h2 className="pr-1 text-lg">About jonnity</h2>
-            <SNSLogo serviceName="github" />
-            <SNSLogo serviceName="x" />
-            <SNSLogo serviceName="threads" />
-          </div>
-          <hr className="my-1 h-px border-0 bg-gray-500" />
-          <div className="p-2">
-            <p>個人開発やアリ飼育などをして生きています。ゆゆ式が好きです。</p>
-            <p>
-              このサイトは、エンジニア的なアウトプットを中心とした雑多な文章を自由に書ける場所として作りました。
-            </p>
-          </div>
-        </aside>
-      </div>
+      <main>{children}</main>
     </>
   );
 }
