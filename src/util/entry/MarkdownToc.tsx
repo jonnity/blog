1b4027e40cb0783504e57c584f308281012ecba3
jonnity <jonny1996_ty@yahoo.co.jp>
2023-12-23 15:10:09 +0900

    目次にはh3も掲載するように追加

--- src/util/entry/MarkdownToc.tsx
+++ src/util/entry/MarkdownToc.tsx
@@ -8,12 +8,20 @@ export const MarkdownToc: React.FC<EntryProp> = ({ entry }) => {
   return (
     <Markdown
       remarkPlugins={[remarkGfm]}
-      allowedElements={["h2"]}
+      allowedElements={["h2", "h3"]}
       components={{
         h2: (node, ..._rest) => {
           if (typeof node?.children === "string")
             return (
               <p className="pl-4 -indent-4 before:content-['・']">
+                <Link href={`#${node.children}`}>{node.children}</Link>
+              </p>
+            );
+        },
+        h3: (node, ..._rest) => {
+          if (typeof node?.children === "string")
+            return (
+              <p className="pl-6 -indent-3 text-sm before:content-['-_']">
                 <Link href={`#${node.children}`}>{node.children}</Link>
               </p>
             );
