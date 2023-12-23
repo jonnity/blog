18623f0f04c99ffa3af04ee880ea6ee41cdd0a5f
jonnity <jonny1996_ty@yahoo.co.jp>
2023-12-23 17:52:56 +0900

    metadataは非公開でも普通にパースするように

--- src/util/entry/EntryMetadata.ts
+++ src/util/entry/EntryMetadata.ts
@@ -13,46 +13,30 @@ const pastDateStringSchema = z.string().transform((dateStr, ctx) => {
   }
   return inputtedDate;
 });
-const defaultParam = {
-  title: "default title",
-  createdAt: new Date(0),
-  tags: [],
-  description: "個人開発の進捗報告や、アリ飼育の様子などを書くブログサイトです",
-};
 
 const titleSchema = z.string().min(1);
 const tagsSchema = z.string().array().optional().default([]);
-const descriptionSchema = z.string();
-const metadata = z.object({
-  title: titleSchema,
-  createdAt: pastDateStringSchema,
-  tags: tagsSchema,
-  description: descriptionSchema.optional(),
-  updatedAt: pastDateStringSchema.optional(),
-});
-type Metadata = z.infer<typeof metadata>;
+const descriptionSchema = z.string().optional();
+const createdAtSchema = pastDateStringSchema;
+const updatedAtSchema = pastDateStringSchema.optional();
 
-const metadataWithCatch = z
-  .object({
-    title: titleSchema.catch(defaultParam.title),
-    createdAt: pastDateStringSchema.catch(defaultParam.createdAt),
-    tags: tagsSchema.catch(defaultParam.tags),
-    description: descriptionSchema,
-  })
-  .catch(defaultParam);
 export class EntryMetadata {
   public readonly isPublic: boolean;
-  public readonly metadata: Metadata;
+  public readonly title: z.infer<typeof titleSchema>;
+  public readonly createdAt: z.infer<typeof createdAtSchema>;
+  public readonly tags: z.infer<typeof tagsSchema>;
+  public readonly description: z.infer<typeof descriptionSchema>;
+  public readonly updatedAt: z.infer<typeof updatedAtSchema>;
   constructor(
     public readonly slug: string,
     mdString: string,
   ) {
-    const parsedData = frontMatter<any>(mdString);
-    this.isPublic = !!parsedData.attributes?.isPublic;
-    this.metadata = this.isPublic
-      ? metadata.parse(parsedData.attributes)
-      : metadataWithCatch.parse(parsedData.attributes);
+    const { attributes } = frontMatter<any>(mdString);
+    this.isPublic = !!attributes?.isPublic;
+    this.title = titleSchema.parse(attributes.title);
+    this.createdAt = createdAtSchema.parse(attributes.createdAt);
+    this.tags = tagsSchema.parse(attributes.tags);
+    this.description = descriptionSchema.parse(attributes.description);
+    this.updatedAt = updatedAtSchema.parse(attributes.updatedAt);
   }
-
-  static defaultDescription = defaultParam.description;
 }
--- src/util/metaTagInfo.ts
+++ src/util/metaTagInfo.ts
@@ -5,4 +5,5 @@ export const titleParam = {
   default: defaultTitle,
   template: `%s | ${defaultTitle}`,
 };
-export const defaultDescription = EntryMetadata.defaultDescription;
+export const defaultDescription =
+  "個人開発の進捗報告や、アリ飼育の様子などを書くブログサイトです";
