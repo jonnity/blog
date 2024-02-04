0338715dceb94cdf23025f962611e886b7f1a2ec
jonnity <jonny1996_ty@yahoo.co.jp>
2024-02-04 16:13:14 +0900

    settingMDX

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withMDX = require("@next/mdx")();
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
+  pageExtensions: ["mdx", "ts", "tsx"],
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
