fe2dd170696f4abcb5f3ba11f9171ce8a3f844b9
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-09 00:50:45 +0900

    レスポンシブ対応を三段階にしてさらに調整

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,7 +10,7 @@ export default async function BlogPage() {
 
   return (
     <div className="flex w-full justify-center">
-      <div className="contents-base my-2 flex w-[368px] flex-col justify-start gap-x-4 p-4 backdrop-blur-lg lg:w-[1008px]">
+      <div className="contents-base my-2 flex w-[368px] flex-col justify-start gap-x-4 p-4 backdrop-blur-lg md:w-[752px] lg:w-[880px]">
         <h2 className="text-2xl lg:text-3xl">記事一覧</h2>
         <hr className="mb-1 w-full border-gray-400" />
         <div className="flex flex-wrap gap-4 p-4">
--- src/util/entry/components/EntryLink.tsx
+++ src/util/entry/components/EntryLink.tsx
@@ -9,8 +9,8 @@ export const EntryLink: React.FC<EntryProp> = ({ entry }) => {
   return (
     <>
       <Link href={`./blog/${entry.slug}`}>
-        <article className="h-fit w-36 lg:w-44">
-          <div className="relative h-28 w-full lg:h-32">
+        <article className="h-fit w-36 md:w-40 lg:w-48">
+          <div className="relative h-28 w-full md:h-32 lg:h-36">
             <Image
               src={entry.getThumbnail().url}
               alt={entry.getThumbnail().alt}
@@ -19,7 +19,7 @@ export const EntryLink: React.FC<EntryProp> = ({ entry }) => {
             ></Image>
           </div>
           <div className="p-2">
-            <h3 className="text-base font-bold lg:text-xl">
+            <h3 className="text-base font-bold md:text-lg lg:text-xl">
               {entry.metadata.title}
             </h3>
             <DateInfoSpan
