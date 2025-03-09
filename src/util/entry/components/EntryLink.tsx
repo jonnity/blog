535648705e4ac3c77feeb1f20b1742310efb2bcb
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-09 10:22:01 +0900

    getThumbnail()を一度だけ呼び出すように変更

--- src/util/entry/components/EntryLink.tsx
+++ src/util/entry/components/EntryLink.tsx
@@ -6,17 +6,13 @@ import { TagListSpan } from "./TagList";
 import { DateInfoSpan } from "./DateInfoSpan";
 
 export const EntryLink: React.FC<EntryProp> = ({ entry }) => {
+  const { url, alt } = entry.getThumbnail();
   return (
     <>
       <Link href={`/blog/${entry.slug}`}>
         <article className="h-fit w-36 md:w-40 lg:w-48">
           <div className="relative h-[108px] w-full md:h-[120px] lg:h-36">
-            <Image
-              src={entry.getThumbnail().url}
-              alt={entry.getThumbnail().alt}
-              className="object-cover"
-              fill
-            ></Image>
+            <Image src={url} alt={alt} className="object-cover" fill></Image>
           </div>
           <div className="p-2">
             <h3 className="text-base font-bold md:text-lg lg:text-xl">
