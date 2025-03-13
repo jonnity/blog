d94ed0ad11f900aa0a480687e37ea603a66017f1
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-14 00:19:39 +0900

    トップページをworkのブロックまで作成

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -1,36 +1,63 @@
 import { SNSLogo } from "@/util/profile/SNSLogo";
 import { LatestEntries } from "@/util/entry/components/LatestEntries";
+import { WorkManager } from "@/util/work/Work";
+
+const workManager = WorkManager.getInstance();
+const allWorks = workManager.getWorkList();
+const twoWorks = allWorks.slice(0, 2);
 
 export default function Home() {
   return (
     <div className="flex w-full justify-center">
-      <main className="contents-base my-2 flex w-11/12 flex-col gap-4 p-4 shadow-xl lg:m-8 lg:w-3/5 xl:w-1/2">
-        <section className="entry-base">
-          <h2 className="flex items-center gap-4 text-2xl lg:text-3xl">
-            <span>About Me</span>
+      <main className="contents-base m-2 w-[360px] gap-x-4 p-4 md:w-[720px] lg:w-[960px]">
+        <RouteBlock>
+          <div className="flex gap-4">
+            <HeadSubject>About Me</HeadSubject>
             <div className="flex items-center gap-2">
               <SNSLogo serviceName="github" />
               <SNSLogo serviceName="x" />
               <SNSLogo serviceName="threads" />
             </div>
-          </h2>
-          <p>
-            個人開発やアリ飼育などをしながら、エンジニアとして働いてます。ゆゆ式が好きです。
-            このサイトは、主にエンジニアとしての活動のアプトプットの場として作りました。オモコロとかDPZとかのおもしろ文章を読むのも好きなので、そういうのも出していきたい気持ちはあります。
-          </p>
-          <p>
-            仕事ではほとんどコーディングしないので大変。OSSのソースとかをちゃんと読めばなんとかなる気はしてきてはいます。
-          </p>
-          <p>妻とアリ200匹くらい (2025/02現在) と一緒に暮らしています。</p>
-        </section>
-        <section className="entry-base">
+          </div>
+          <div className="ml-2">
+            <p>
+              個人開発やアリ飼育などをしながら、エンジニアとして働いてます。ゆゆ式が好きです。
+            </p>
+            <p>
+              このサイトは、主にエンジニアとしての活動のアプトプットの場として作りました。
+            </p>
+          </div>
+          <div className="entry-base flex w-full justify-end">
+            <a href="/profile">Read more...</a>
+          </div>
+        </RouteBlock>
+        <RouteBlock>
+          <HeadSubject>Work</HeadSubject>
+          <div className="flex justify-between px-2">
+            {twoWorks.map((work) => {
+              return (
+                <WorkLink
+                  key={work.slug}
+                  slug={work.slug}
+                  title={work.metadata.title}
+                  thumbnail={work.getThumbnail()}
+                />
+              );
+            })}
+          </div>
+          <div className="entry-base flex w-full justify-end">
+            <a href="/work">See more...</a>
+          </div>
+        </RouteBlock>
+
+        {/* <section className="entry-base">
           <h2 className="text-2xl lg:text-3xl">News</h2>
           <p>
-            <a href="https://sketch-match.jonnity.com" target="_blank">
-              Sketch Match
-            </a>
-            というお絵かき + 神経衰弱のパーティゲームを作りました。
-            ペアになっているお題が各プレイヤーに出され、その絵を使って神経衰弱をするゲームをブラウザで遊べるので↑のリンクからぜひ。
+          <a href="https://sketch-match.jonnity.com" target="_blank">
+          Sketch Match
+          </a>
+          というお絵かき + 神経衰弱のパーティゲームを作りました。
+          ペアになっているお題が各プレイヤーに出され、その絵を使って神経衰弱をするゲームをブラウザで遊べるので↑のリンクからぜひ。
           </p>
         </section>
         <section>
@@ -43,7 +70,7 @@ export default function Home() {
           <div className="entry-base">
             <h2 className="text-2xl lg:text-3xl">Works</h2>
           </div>
-        </section>
+        </section> */}
         {/* <section>
           <h2 className="text-2xl lg:text-3xl">記事一覧</h2>
           <div className="flex flex-wrap gap-4 p-4">
@@ -56,3 +83,36 @@ export default function Home() {
     </div>
   );
 }
+
+const HeadSubject: React.FC<{ children?: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <h3 className="flex items-center gap-4 text-xl font-bold md:text-2xl">
+      {children}
+    </h3>
+  );
+};
+
+const WorkLink: React.FC<{
+  slug: string;
+  thumbnail: { url: string; alt: string };
+  title: string;
+}> = ({ slug, thumbnail, title }) => {
+  return (
+    <a href={`/work/${slug}`}>
+      <article className="flex h-fit w-[148px] flex-col items-center">
+        <img
+          src={thumbnail.url}
+          alt={thumbnail.alt}
+          className="h-[111px] w-[148px] object-cover"
+        />
+        <h3 className="text-base font-bold lg:text-xl">{title}</h3>
+      </article>
+    </a>
+  );
+};
+
+const RouteBlock: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <div className="h-52">{children}</div>;
+};
