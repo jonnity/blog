92213b2584b9eab1ce4b919d0b75859517c7be1e
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-14 22:28:20 +0900

    コメントアウトを削除

--- src/app/(top)/page.tsx
+++ src/app/(top)/page.tsx
@@ -96,36 +96,6 @@ export default function Home() {
             <Link href="/blog">Read more...</Link>
           </div>
         </RouteBlock>
-
-        {/* <section className="entry-base">
-          <h2 className="text-2xl lg:text-3xl">News</h2>
-          <p>
-          <a href="https://sketch-match.jonnity.com" target="_blank">
-          Sketch Match
-          </a>
-          というお絵かき + 神経衰弱のパーティゲームを作りました。
-          ペアになっているお題が各プレイヤーに出され、その絵を使って神経衰弱をするゲームをブラウザで遊べるので↑のリンクからぜひ。
-          </p>
-        </section>
-        <section>
-          <div className="entry-base">
-            <h2 className="text-2xl lg:text-3xl">Blogs</h2>
-          </div>
-          <LatestEntries numOfEntries={3} />
-        </section>
-        <section>
-          <div className="entry-base">
-            <h2 className="text-2xl lg:text-3xl">Works</h2>
-          </div>
-        </section> */}
-        {/* <section>
-          <h2 className="text-2xl lg:text-3xl">記事一覧</h2>
-          <div className="flex flex-wrap gap-4 p-4">
-            {entryManager.getEntryList().map((entry) => (
-              <EntryLink key={entry.slug} entry={entry} />
-            ))}
-          </div>
-        </section> */}
       </main>
     </div>
   );
