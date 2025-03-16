46160b26a954a4b3a425455d14283d1e8eb0adbb
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-16 17:54:14 +0900

    blogを見てるときにmonthlyでも起きる問題が発覚したので修正

--- src/app/monthly/[year-month]/page.tsx
+++ src/app/monthly/[year-month]/page.tsx
@@ -37,7 +37,7 @@ export default async function Page({
 
   return (
     <>
-      <div className="m-4 flex flex-col gap-4 justify-self-center md:w-[736px] md:flex-row lg:w-[1000px]">
+      <div className="m-4 flex w-[97%] flex-col gap-4 justify-self-center md:w-[736px] md:flex-row lg:w-[1000px]">
         <article className="w-full md:w-[440px] lg:w-[664px]">
           <BlogEntry entry={entry} />
         </article>
