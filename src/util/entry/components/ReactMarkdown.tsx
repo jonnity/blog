08ed66bb305937ef8b646a925246ec3852ef414b
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-16 23:01:56 +0900

    follow updating `react-markdown` to remove className (see also https://github.com/remarkjs/react-markdown/blob/main/changelog.md#1000---2025-02-20).

--- src/util/entry/components/ReactMarkdown.tsx
+++ src/util/entry/components/ReactMarkdown.tsx
@@ -13,105 +13,108 @@ type ReactMarkdownProps = { mdBody: string };
 
 export const ReactMarkdown: React.FC<ReactMarkdownProps> = ({ mdBody }) => {
   return (
-    <Markdown
-      remarkPlugins={[remarkGfm]}
-      className="entry-base"
-      urlTransform={(url, _key, { tagName }) => {
-        if (tagName === "img") {
-          return /^https?:/.test(url) ? url : `/entry/${url}`;
-        }
-        return url;
-      }}
-      components={{
-        p({ node, ...rest }) {
-          const defaultParagraph = <p>{rest.children}</p>;
-          const firstChild = node?.children[0];
-          const secondChild = node?.children[1];
-          const firstChildIsImage =
-            firstChild?.type === "element" && firstChild.tagName === "img";
-          const secondChildIsText = secondChild?.type === "text";
-          if (
-            typeof node?.children?.length !== "number" ||
-            node.children.length !== 2 ||
-            !firstChildIsImage ||
-            !secondChildIsText
-          ) {
-            return defaultParagraph;
+    <div className="entry-base">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        urlTransform={(url, _key, { tagName }) => {
+          if (tagName === "img") {
+            return /^https?:/.test(url) ? url : `/entry/${url}`;
           }
+          return url;
+        }}
+        components={{
+          p({ node, ...rest }) {
+            const defaultParagraph = <p>{rest.children}</p>;
+            const firstChild = node?.children[0];
+            const secondChild = node?.children[1];
+            const firstChildIsImage =
+              firstChild?.type === "element" && firstChild.tagName === "img";
+            const secondChildIsText = secondChild?.type === "text";
+            if (
+              typeof node?.children?.length !== "number" ||
+              node.children.length !== 2 ||
+              !firstChildIsImage ||
+              !secondChildIsText
+            ) {
+              return defaultParagraph;
+            }
 
-          const src = srcSchema.parse(firstChild.properties.src);
-          const alt = altSchema.parse(firstChild.properties.alt);
-          return (
-            <Suspense>
-              <ImageViewer src={src} alt={alt} caption={secondChild.value} />
-            </Suspense>
-          );
-        },
-        a(prop) {
-          const { node, href, ...rest } = prop;
-          if (node?.children.length === 0) {
-            throw new Error(`Titleの指定のないリンクがある: ${href}`);
-          }
-          const target = /^https?:/.test(prop.href || "") ? "_blank" : "_self";
-          return <a href={href} target={target} {...rest} />;
-        },
-        pre({ node, children, ...rest }) {
-          const codeNode = node?.children[0];
-          const hasCodeChildOnly =
-            node?.children.length === 1 &&
-            codeNode?.type === "element" &&
-            codeNode?.tagName === "code";
-          if (!hasCodeChildOnly) {
-            return <pre {...rest}>{children}</pre>;
-          }
+            const src = srcSchema.parse(firstChild.properties.src);
+            const alt = altSchema.parse(firstChild.properties.alt);
+            return (
+              <Suspense>
+                <ImageViewer src={src} alt={alt} caption={secondChild.value} />
+              </Suspense>
+            );
+          },
+          a(prop) {
+            const { node, href, ...rest } = prop;
+            if (node?.children.length === 0) {
+              throw new Error(`Titleの指定のないリンクがある: ${href}`);
+            }
+            const target = /^https?:/.test(prop.href || "")
+              ? "_blank"
+              : "_self";
+            return <a href={href} target={target} {...rest} />;
+          },
+          pre({ node, children, ...rest }) {
+            const codeNode = node?.children[0];
+            const hasCodeChildOnly =
+              node?.children.length === 1 &&
+              codeNode?.type === "element" &&
+              codeNode?.tagName === "code";
+            if (!hasCodeChildOnly) {
+              return <pre {...rest}>{children}</pre>;
+            }
 
-          const code =
-            codeNode.children[0].type === "text"
-              ? codeNode.children[0].value
+            const code =
+              codeNode.children[0].type === "text"
+                ? codeNode.children[0].value
+                : "";
+
+            const codeClassName = Array.isArray(codeNode.properties.className)
+              ? codeNode.properties.className[0].toString()
               : "";
-
-          const codeClassName = Array.isArray(codeNode.properties.className)
-            ? codeNode.properties.className[0].toString()
-            : "";
-          const [languageInfo, filename] = codeClassName.split(":");
-          const language = languageInfo.replace("language-", "");
-          if (language === "youtube") {
-            return <IframeYoutubePlayer videoId={code} />;
-          }
-          if (language === "twitter") {
-            return <EmbeddedTweet id={code.trim()} />;
-          }
-          return (
-            <SyntaxHighlightedCodeBlock
-              code={code.replace(/\n$/, "")}
-              language={language}
-              filename={filename}
-            />
-          );
-        },
-        h2(node) {
-          const id = `${node.children}`;
-          return !node?.children ? <></> : <h2 id={id}>{node.children}</h2>;
-        },
-        h3(node) {
-          const id = `${node.children}`;
-          return !node?.children ? <></> : <h3 id={id}>{node.children}</h3>;
-        },
-        h4(node) {
-          const id = `${node.children}`;
-          return !node?.children ? <></> : <h4 id={id}>{node.children}</h4>;
-        },
-        h5(node) {
-          const id = `${node.children}`;
-          return !node?.children ? <></> : <h5 id={id}>{node.children}</h5>;
-        },
-        h6(node) {
-          const id = `${node.children}`;
-          return !node?.children ? <></> : <h6 id={id}>{node.children}</h6>;
-        },
-      }}
-    >
-      {mdBody}
-    </Markdown>
+            const [languageInfo, filename] = codeClassName.split(":");
+            const language = languageInfo.replace("language-", "");
+            if (language === "youtube") {
+              return <IframeYoutubePlayer videoId={code} />;
+            }
+            if (language === "twitter") {
+              return <EmbeddedTweet id={code.trim()} />;
+            }
+            return (
+              <SyntaxHighlightedCodeBlock
+                code={code.replace(/\n$/, "")}
+                language={language}
+                filename={filename}
+              />
+            );
+          },
+          h2(node) {
+            const id = `${node.children}`;
+            return !node?.children ? <></> : <h2 id={id}>{node.children}</h2>;
+          },
+          h3(node) {
+            const id = `${node.children}`;
+            return !node?.children ? <></> : <h3 id={id}>{node.children}</h3>;
+          },
+          h4(node) {
+            const id = `${node.children}`;
+            return !node?.children ? <></> : <h4 id={id}>{node.children}</h4>;
+          },
+          h5(node) {
+            const id = `${node.children}`;
+            return !node?.children ? <></> : <h5 id={id}>{node.children}</h5>;
+          },
+          h6(node) {
+            const id = `${node.children}`;
+            return !node?.children ? <></> : <h6 id={id}>{node.children}</h6>;
+          },
+        }}
+      >
+        {mdBody}
+      </Markdown>
+    </div>
   );
 };
