30c50b48af3820bdd429e1fa797f5c91404510ee
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-16 17:53:52 +0900

    blogエントリページのレスポンシブ対応を修正

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -34,11 +34,11 @@ export default async function Page({
   const entry = entryManager.getEntry(slug);
   return (
     <>
-      <div className="m-4 flex flex-col justify-center gap-4 lg:mx-0 lg:flex-row">
-        <article className="w-full lg:w-3/5 xl:w-1/2">
+      <div className="m-4 flex w-[97%] flex-col gap-4 justify-self-center md:w-[736px] md:flex-row lg:w-[1000px]">
+        <article className="w-full md:w-[440px] lg:w-[664px]">
           <BlogEntry entry={entry} />
         </article>
-        <aside className="w-full lg:w-1/4 xl:w-1/5">
+        <aside className="w-full md:w-[280px] lg:w-[320px]">
           <SideBarInfo mdBody={entry.body} />
         </aside>
         <Hamburger>
