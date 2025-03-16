292d935b3ab66bd9e6cf25dbd24baab94a509922
jonnity <jonny1996_ty@yahoo.co.jp>
2025-03-16 16:50:02 +0900

    MonthlySelectorのパーツを再利用できるように細分化

--- src/app/monthly/components/MonthlySelector.tsx
+++ src/app/monthly/components/MonthlySelector.tsx
@@ -1,5 +1,4 @@
-"use client";
-import { redirect } from "next/navigation";
+import { MonthlyParts } from "./MonthlyParts";
 
 type MonthlySelectorProp = {
   yearMonthList: string[];
@@ -10,39 +9,14 @@ export const MonthlySelector: React.FC<MonthlySelectorProp> = ({
   yearMonthList,
   currentYearMonth,
 }) => {
-  const selectorId = "yearMonthSelector";
-  const yearMonthHandler = () => {
-    const selector = document.getElementById(selectorId) as HTMLSelectElement;
-    redirect(`/monthly/${selector.value}`);
-  };
   return (
     <div className="contents-base flex flex-col p-2">
       <h2 className="p-2 text-lg">過去の月記</h2>
       <hr className="my-1 w-full border-gray-400" />
-      <div className="m-0 flex justify-end gap-2">
-        <select
-          name="yearMonth"
-          className="rounded border p-2"
-          defaultValue={currentYearMonth}
-          id={selectorId}
-        >
-          {yearMonthList.map((yearMonth) => {
-            const [year, month] = yearMonth.split("-");
-            return (
-              <option key={yearMonth} value={yearMonth}>
-                {year}年{month}月
-              </option>
-            );
-          })}
-        </select>
-        <button
-          className="rounded border bg-gray-200 p-1"
-          onClick={yearMonthHandler}
-        >
-          表示
-        </button>
-        <input type="hidden" name="redirect" value="true" />
-      </div>
+      <MonthlyParts
+        yearMonthList={yearMonthList}
+        currentYearMonth={currentYearMonth}
+      />
     </div>
   );
 };
