6afc71ff2f265b3f876319981cef3ce34e786b54
jonnity <jonny1996_ty@yahoo.co.jp>
2025-06-01 11:31:19 +0900

    avoid to extend backgroundImage in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,6 @@ const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "ant-nest_landscape": "url('/bg-ant-nest_landscape.webp')",
-        "ant-nest_portrait": "url('/bg-ant-nest_portrait.webp')",
-      },
       colors: {
         "monokai-filename": {
           bg: "#1d1e1a",
